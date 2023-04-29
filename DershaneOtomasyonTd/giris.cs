58fd881929941e54050aa96013fe941ff674674a
PC <osman.sengun01@gmail.com>
2023-04-29 21:49:42 +0300

    giriş kısmı çözüldü.Tamamen bitti

--- DershaneOtomasyonTd/giris.cs
+++ DershaneOtomasyonTd/giris.cs
@@ -9,14 +9,18 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
 using System.Collections;
+using System.Reflection.Emit;
 
 namespace DershaneOtomasyonTd
 {
     public partial class giris : Form
     {
-        public giris()
+        ArrayList listcaptcha = new ArrayList();    
+        public  giris()
         {
             InitializeComponent();
+            string captcha = captchaMetod();
+            listcaptcha.Add(captcha);
         }
         SqlConnection bag = new SqlConnection("Data Source=DESKTOP-CBUGCJB\\SQLEXPRESS;Initial Catalog=dershane2;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
         private void bunifuImageButton1_Click(object sender, EventArgs e)
@@ -38,6 +42,7 @@ namespace DershaneOtomasyonTd
         {
             string no = kulpersno.Text;
             string sifre2 = sifretext.Text;
+            string captcha = listcaptcha[0].ToString();
 
             string sorgu = "SELECT Sifre, KullanıcıAdı , Yetki FROM tblAdminPanel WHERE KulPersNo = @No";
 
@@ -64,25 +69,83 @@ namespace DershaneOtomasyonTd
                         {
                             case "Yönetici":
                                 yoneticipanel yoneticipanel = new yoneticipanel();
-                                yoneticipanel.Show();
+                                if (captcha == guvenliktextb.Text)
+                                {
+                                    yoneticipanel.Show();
+                                }
+                                else
+                                {
+                                    MessageBox.Show("Güvenlik Kodunu Yanlış Girdiniz.");
+                                }
                                 break;
                             case "Öğrenci":
                                 ogrencipanel ogrencipanel = new ogrencipanel();
-                                ogrencipanel.Show();
+                                if (captcha == guvenliktextb.Text)
+                                {
+                                    ogrencipanel.Show();
+                                }
+                                else
+                                {
+                                    MessageBox.Show("Güvenlik Kodunu Yanlış Girdiniz.");
+                                }
                                 break;
                             case "Öğretmen":
                                 ogretmenpanel ogretmenpanel= new ogretmenpanel();
-                                ogretmenpanel.Show();
+                                if (captcha == guvenliktextb.Text)
+                                {
+                                    ogretmenpanel.Show();
+                                }
+                                else
+                                {
+                                    MessageBox.Show("Güvenlik Kodunu Yanlış Girdiniz.");
+                                }
                                 break;
                         }
-                        bag.Close();
                     }
                     else
                     {
                         MessageBox.Show("Şifreyi Yanlış");
                     }
                 }
+                if(bag.State.ToString() == "Open")
+                {
+                    bag.Close();    
+                }
+                else
+                {
+                    bag.Close();
+                    bag.Open();
+                }
+                bag.Close();
             }
+        }
+
+        private void bunifuLabel1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+        public string captchaMetod()
+        {
+            Random random = new Random();
+            string captcha = "";
+            for (int i = 0; i < 6; i++) // 6 karakterlik rastgele bir dize oluşturulur.
+            {
+                int num = random.Next(0, 10); // 0'dan 9'a kadar rastgele bir sayı üretir.
+                captcha += num.ToString();
+            }
+
+            for (int i = 0; i < 2; i++) // 2 karakterlik rastgele bir dize oluşturulur.
+            {
+                char ch = Convert.ToChar(Convert.ToInt32(Math.Floor(26 * random.NextDouble() + 65))); // A'dan Z'ye kadar rastgele bir harf üretir.
+                captcha += ch.ToString();
+                captchatextb.Text = captcha;
+            }
+            return captcha;
+        }
+
+        private void captcha_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
